0285bf3f41ae04b8d4606f3753d7a8a0bfc41dd7
Josh Castillo <John.Joshua.Castillo@gmail.com>
2021-10-18 12:24:18 -0400

    add 2 empty deck variables, a main card variable along with cached element references for those 3. add 5 event listeners for 3 variables and 2 buttons for start and stop (use for timer)

--- js/app.js
+++ js/app.js
@@ -2,5 +2,19 @@
 
 let whtDeck = []
 let blkDeck = []
+let mainCard = []
 
 let whtDeckEl = document.getElementById('whtDeck')
+let blkDeckEl = document.getElementById('blkDeck')
+let mainCardEl = document.getElementById('mainCard')
+
+document.getElementById('whtDeck').addEventListener('click',() => {
+  console.log('clicked white deck')})
+document.getElementById('blkDeck').addEventListener('click',() => {
+  console.log('clicked black deck')})
+document.getElementById('mainCard').addEventListener('click',() => {
+  console.log('clicked main card')})
+document.getElementById('startBtn').addEventListener('click',() => {
+  console.log('clicked start button')})
+document.getElementById('stopBtn').addEventListener('click',() => {
+  console.log('clicked stop button')})
