3df01c2f6f45636a14132d6bfa09e3e548a08053
Josh Castillo <John.Joshua.Castillo@gmail.com>
2021-10-18 21:34:14 -0400

    just to switch devices and make sure file is up to date

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ let blkDeckEl = document.getElementById('blkDeck')
 let mainCardEl = document.getElementById('mainCard')
 
 document.getElementById('whtDeck').addEventListener('click',() => {
-  console.log('clicked white deck')})
+  document.querySelectorAll('cardSet')})
 document.getElementById('blkDeck').addEventListener('click',() => {
   console.log('clicked black deck')})
 document.getElementById('mainCard').addEventListener('click',() => {
@@ -26,4 +26,6 @@ function init() {
   whtDeck = ['w00','w01','w02','w03','w04','w10','w11','w12','w13','w14','w20','w21','w22','w23','w24','w30','w31','w32','w33','w34','w40','w41','w42','w43','w44']
   blkDeck = ['b00','b01','b02','b03','b04','b10','b11','b12','b13','b14','b20','b21','b22','b23','b24','b30','b31','b32','b33','b34','b40','b41','b42','b43','b44']
 }
-function handleClick()
+// function render(themePicked) {
+//   if (whtDeckEl)
+// }
