e18e236631136a83300b19eae910964ae16dec1d
Josh Castillo <John.Joshua.Castillo@gmail.com>
2021-10-19 18:28:33 -0400

    add timer

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 let whtTheme = []
 let blkTheme = []
 let mainCard = []
+let timerIntervalId
+let min, sec, seconds = 0
 /*------------- Variables (state) -------------*/
 
 /*--------- Cached Element References ---------*/
@@ -12,7 +14,8 @@ mainCardEl = document.getElementById('mainCard')
 cardGridEl = document.getElementById('cardGrid')
 cardsEl = document.getElementById('card')
 startBtnEl = document.getElementById('startBtn')
-startBtnEl = document.getElementById('startBtn')
+resetBtnEl = document.getElementById('resetBtn')
+timerEl = document.getElementById('timer')
 
 /*-------------- Event Listeners --------------*/
 
@@ -35,11 +38,23 @@ cardGridEl.addEventListener('click',(evt) => {
 mainCardEl.addEventListener('click',() => {
   console.log('clicked main card')})
 
-document.getElementById('startBtn').addEventListener('click',() => {
-  console.log('clicked start button')})
+startBtnEl.addEventListener('click',() => {
+  startBtnEl.textContent = (startBtnEl.textContent === 'Start') ? 'Stop' : 'Start'
+  if (timerIntervalId) {
+    clearInterval(timerIntervalId)
+    timerIntervalId = null
+  } else {
+  startTimer();
+  }
+})
 
-document.getElementById('stopBtn').addEventListener('click',() => {
-  console.log('clicked stop button')})
+resetBtnEl.addEventListener('click',() => {
+  startBtnEl.textContent = 'Start'
+  clearInterval(timerIntervalId)
+  seconds = 0
+  timerIntervalId = null
+  render()
+})
 /*----------------- Functions -----------------*/
 
 
@@ -48,11 +63,24 @@ document.getElementById('stopBtn').addEventListener('click',() => {
 
 init ()
 
+function tick() {
+  seconds++
+  render()
+}
+
+function startTimer(){
+  timerIntervalId = setInterval(tick, 1000)
+}
+
 function init() {
   whtDeck = ['w00','w01','w02','w03','w04','w10','w11','w12','w13','w14','w20','w21','w22','w23','w24','w30','w31','w32','w33','w34','w40','w41','w42','w43','w44']
   blkDeck = ['b00','b01','b02','b03','b04','b10','b11','b12','b13','b14','b20','b21','b22','b23','b24','b30','b31','b32','b33','b34','b40','b41','b42','b43','b44']
 }
 
 function render() {
-  
+  min = Math.floor(seconds / 60);
+  hr = Math.floor(seconds / 3600);
+  sec = seconds % 60;
+  min = min % 60;
+  (sec < 10) ? timerEl.innerText = `${min}:0${sec}` : timerEl.innerText = `${min}:${sec}`
 }
